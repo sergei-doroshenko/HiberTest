03b2e2953ddaade5ba96558bf8a42c7de5315d87
sergei-doroshenko <6823298@gmail.com>
2015-09-02 09:53:04 +0300

    Resource cost changes.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -66,3 +66,66 @@ CREATE TABLE res_skills
   CONSTRAINT skill_skilltype_fk FOREIGN KEY (skill_id) REFERENCES skill_types (id)
 );
 
+CREATE TABLE currency
+(
+  id bigint NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  letter_code VARCHAR(3) NOT NULL,
+  digit_code VARCHAR(3) NOT NULL,
+  symbol VARCHAR(5) NOT NULL,
+  default_currency boolean NOT NULL,
+  CONSTRAINT currency_pkey PRIMARY KEY (id),
+  CONSTRAINT currency_digit_code_key UNIQUE (digit_code),
+  CONSTRAINT currency_letter_code_key UNIQUE (letter_code),
+  CONSTRAINT currency_symbol_key UNIQUE (symbol)
+);
+
+CREATE TABLE resource
+(
+  id bigint NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  native_name VARCHAR(255),
+  login VARCHAR(255),
+  active boolean DEFAULT true,
+  sex boolean DEFAULT true,
+  email VARCHAR(255),
+  notify_unfilled_ts boolean NOT NULL,
+  emp_currency bigint NOT NULL,
+  nwo_prohibited boolean,
+  CONSTRAINT resource_pkey PRIMARY KEY (id),
+  CONSTRAINT resource_emp_currency_fkey FOREIGN KEY (emp_currency) REFERENCES currency (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT resource_login_unique UNIQUE (login)
+);
+
+CREATE TABLE resource_cost
+(
+  id bigint NOT NULL,
+  start_date date NOT NULL,
+  resource_id bigint NOT NULL,
+  base_cost BIGINT NOT NULL,
+  currency_id bigint NOT NULL,
+  CONSTRAINT resource_cost_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_currency FOREIGN KEY (currency_id) REFERENCES currency (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT resource_cost_resource_id_fkey FOREIGN KEY (resource_id) REFERENCES resource (id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE exchange_currency_rates
+(
+  id bigint NOT NULL,
+  date timestamp NOT NULL,
+  currency_from bigint NOT NULL,
+  currency_to bigint NOT NULL,
+  exchange_rate numeric(18,6) NOT NULL,
+  CONSTRAINT exchange_currency_rates_pkey PRIMARY KEY (id),
+  CONSTRAINT exchange_currency_rates_currency_from_fkey FOREIGN KEY (currency_from)
+  REFERENCES currency (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT exchange_currency_rates_currency_to_fkey FOREIGN KEY (currency_to)
+  REFERENCES currency (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+
